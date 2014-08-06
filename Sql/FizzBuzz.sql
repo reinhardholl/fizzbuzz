0b4346680a18a1e31c4d61558d69f9a540035783
Reinhard Holl <xeroo125@gmail.com>
2014-08-06 10:47:43 +0200

    Implement fizzbuzz in tsql

---
+++ Sql/FizzBuzz.sql
@@ -0,0 +1,28 @@
+CREATE FUNCTION GetTheFizz (@TestValue int)
+RETURNS VARCHAR(10)
+AS BEGIN
+	IF (@TestValue %3 = 0 and @TestValue %5 = 0)
+	BEGIN
+		return 'FizzBuzz'
+	END
+	IF (@TestValue %3 = 0)
+	BEGIN
+		return 'Fizz'
+	END
+	IF (@TestValue %5 = 0)
+	BEGIN
+		return 'Buzz'
+	END
+	return cast(@TestValue as varchar(10))
+END
+GO
+
+DECLARE @i int = 0
+WHILE @i < 100 BEGIN
+    SET @i = @i + 1
+
+	PRINT dbo.GetTheFizz(@i)
+END
+
+
+DROP FUNCTION GetTheFizz
